1b8cec4cf6de66587f50bd768c29ec8e03225b9e
Jacques Kang <jacques.kang@voluntis.com>
2018-11-12 17:42:26 +0100

    add database storage support

---
+++ src/JKang.EventSourcing.Persistence.EfCore/EfEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace JKang.EventSourcing.Persistence.EfCore
+{
+    public class EfEvent
+    {
+        public string Store { get; set; }
+        public string Container { get; set; }
+        public string Key { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
---
+++ src/Samples/Samples.WebApp/Data/SampleDbContext.cs
@@ -0,0 +1,19 @@
+﻿using JKang.EventSourcing.Persistence.EfCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Samples.WebApp.Data
+{
+    public class SampleDbContext : DbContext, IEventSourcingDbContext
+    {
+        public SampleDbContext(DbContextOptions<SampleDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<EfEvent> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EfEventConfiguration());
+        }
+    }
+}
