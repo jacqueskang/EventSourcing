68b966e59a63e8413f4b1d8fb0c9d0f288dc60c8
Jacques Kang <jkang.perso@gmail.com>
2020-02-02 13:50:14 +0100

    refactor: test web app cleanup

--- src/JKang.EventSourcing.TestingWebApp/Startup.cs
+++ src/JKang.EventSourcing.TestingWebApp/Startup.cs
@@ -39,72 +39,82 @@ namespace JKang.EventSourcing.TestingWebApp
 
             switch (persistenceMode)
             {
-                case PersistenceMode.DynamoDB:
-                    if (HostingEnvironment.IsDevelopment())
-                    {
-                        services.AddSingleton<IAmazonDynamoDB>(sp => new AmazonDynamoDBClient(new AmazonDynamoDBConfig
-                        {
-                            ServiceURL = "http://localhost:8800"
-                        }));
-                    }
-                    else
-                    {
-                        services.AddAWSService<IAmazonDynamoDB>();
-                    }
+                case PersistenceMode.FileSystem:
+                    ConfigureServicesForFileSystem(services);
                     break;
                 case PersistenceMode.CosmosDB:
-                    services.AddSingleton(_ =>
-                        new CosmosClientBuilder(Configuration.GetConnectionString("CosmosDB"))
-                            .WithConnectionModeDirect()
-                            .WithCustomSerializer(new EventSourcingCosmosSerializer())
-                            .Build());
+                    ConfigureServicesForCosmosDB(services);
+                    break;
+                case PersistenceMode.DynamoDB:
+                    ConfigureServicesForDynamoDB(services);
                     break;
                 case PersistenceMode.EfCore:
-                    services.AddDbContext<SampleDbContext>(x =>
-                    {
-                        x.UseInMemoryDatabase("eventstore");
-                    });
+                    ConfigureServicesForEfCore(services);
                     break;
                 default:
                     break;
             }
+        }
+
+        public void ConfigureServicesForFileSystem(IServiceCollection services)
+        {
+            services.AddEventSourcing(builder =>
+                builder
+                    .UseTextFileEventStore<GiftCard, Guid>(x => x.Folder = "C:/Temp/GiftcardEvents")
+                    .UseTextFileSnapshotStore<GiftCard, Guid>(x => x.Folder = "C:/Temp/GiftcardEvents"));
+        }
+
+        public void ConfigureServicesForCosmosDB(IServiceCollection services)
+        {
+            services
+                .AddSingleton(_ =>
+                    new CosmosClientBuilder(Configuration.GetConnectionString("CosmosDB"))
+                        .WithConnectionModeDirect()
+                        .WithCustomSerializer(new EventSourcingCosmosSerializer())
+                        .Build())
+                .AddEventSourcing(builder =>
+                    builder
+                        .UseCosmosDBEventStore<GiftCard, Guid>(x =>
+                        {
+                            x.DatabaseId = "EventSourcingTestingWebApp";
+                            x.ContainerId = "GiftcardEvents";
+                        })
+                        .UseCosmosDBSnapshotStore<GiftCard, Guid>(x =>
+                        {
+                            x.DatabaseId = "EventSourcingTestingWebApp";
+                            x.ContainerId = "GiftcardSnapshots";
+                        }));
+        }
+
+        public void ConfigureServicesForDynamoDB(IServiceCollection services)
+        {
+#if DEBUG
+            services.AddSingleton<IAmazonDynamoDB>(sp => new AmazonDynamoDBClient(new AmazonDynamoDBConfig
+            {
+                ServiceURL = "http://localhost:8800"
+            }));
+#else
+            services.AddAWSService<IAmazonDynamoDB>();
+#endif
 
             services.AddEventSourcing(builder =>
             {
-                switch (persistenceMode)
-                {
-                    case PersistenceMode.FileSystem:
-                        builder
-                            .UseTextFileEventStore<GiftCard, Guid>(x => x.Folder = "C:/Temp/GiftcardEvents")
-                            .UseTextFileSnapshotStore<GiftCard, Guid>(x => x.Folder = "C:/Temp/GiftcardEvents")
-                            ;
-                        break;
-                    case PersistenceMode.DynamoDB:
-                        builder
-                            .UseDynamoDBEventStore<GiftCard, Guid>(x => x.TableName = "GiftcardEvents")
-                            .UseDynamoDBSnapshotStore<GiftCard, Guid>(x => x.TableName = "GiftcardSnapshots")
-                            ;
-                        break;
-                    case PersistenceMode.CosmosDB:
-                        builder
-                            .UseCosmosDBEventStore<GiftCard, Guid>(x =>
-                            {
-                                x.DatabaseId = "EventSourcingTestingWebApp";
-                                x.ContainerId = "GiftcardEvents";
-                            })
-                            .UseCosmosDBSnapshotStore<GiftCard, Guid>(x =>
-                            {
-                                x.DatabaseId = "EventSourcingTestingWebApp";
-                                x.ContainerId = "GiftcardSnapshots";
-                            });
-                        break;
-                    case PersistenceMode.EfCore:
-                        builder.UseDbEventStore<SampleDbContext, GiftCard, Guid>();
-                        break;
-                    default:
-                        break;
-                }
+                builder
+                    .UseDynamoDBEventStore<GiftCard, Guid>(x => x.TableName = "GiftcardEvents")
+                    .UseDynamoDBSnapshotStore<GiftCard, Guid>(x => x.TableName = "GiftcardSnapshots")
+                    ;
             });
+        }
+
+        public void ConfigureServicesForEfCore(IServiceCollection services)
+        {
+            services
+                .AddDbContext<SampleDbContext>(x => x.UseInMemoryDatabase("eventstore"))
+                .AddEventSourcing(builder =>
+                {
+                    builder.UseDbEventStore<SampleDbContext, GiftCard, Guid>();
+                })
+            ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
